e8452b324aeacb8f8eb24fdcfca7405afb2184af
Bruno Tavares <bruno.exz@gmail.com>
2013-09-18 20:22:07 -0300

    Parsing a card (only 2 of spades)

--- src/dojo/core.clj
+++ src/dojo/core.clj
@@ -1,7 +1,10 @@
 (ns dojo.core (:use [clojure.string]))
 
+(defn card [string]
+	[({\2 :two} (first string)), :spades])
+
 (defn hand [string]
-  (split string #" "))
+  (map card (split string #" ")))
 
 (defn compare-hands [player1 player2]
 	:first)
--- test/dojo/core_test.clj
+++ test/dojo/core_test.clj
@@ -2,10 +2,27 @@
   (:require [clojure.test :refer :all]
             [dojo.core :refer :all]))
 
+
 (deftest a-hand
   (testing "hand parser"
-    (is (= (list "2c" "2d" "td" "7h") (hand "2c 2d td 7h")))
-    (is (= (list "6c" "9d" "kd" "qh") (hand "6c 9d kd qh")))))
+    (is (= 
+      [
+        [:two, :clubs] 
+        [:two, :diamonds] 
+        [:ten, :diamonds] 
+        [:seven, :hearts] 
+        [:six, :hearts]] 
+      (hand "2c 2d td 7h 6h")))
+    (is (= 
+      [
+        [:six, :clubs]
+        [:nine, :diamonds] 
+        [:king, :diamonds] 
+        [:queen :hearts] 
+        [:king, :diamonds]] 
+      (hand "6c 9d kd qh kd"))))
+  (testing "card parser"
+    (is (= [:two, :spades] (card "2s")))))
 
 #_(deftest a-showdown
   (testing "highest card"
